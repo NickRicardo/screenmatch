3130915bd9434fd7c98312e3a1f2cc7f1c2716cd
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-08-22 15:47:01 -0300

    Inicio: Criando o projeto SCreen macth sem Web

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+//Esta notação é para ignorar tudo aquilo que nao mapeamos dentro do JsonAlias "jackson"
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title")String titulo,
+                         @JsonAlias("totalSeasons")Integer totalTemporadas,
+                         @JsonAlias("imdRating") String avaliacao) {
+}
